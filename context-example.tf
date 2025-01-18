2df714ce4dd6c59b2fe9646eb3abbc9252055501
clauspacelift <clau.ignat46@gmail.com>
2025-01-18 09:07:33 +0200

    update 4

--- context-example.tf
+++
@@ -1,7 +0,0 @@
-#Demonstrate Spacelift context.
-
-#```hcl
-resource "aws_s3_bucket" "context_example" {
-  bucket = "spacelift-context-example-${var.context_env}"
-  acl    = "private"
-}
--- main.tf
+++ main.tf
@@ -1,34 +1,8 @@
-terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-    }
-    }
-}
-
-provider "aws" {
-    region = "us-east-1"
-}
-
-#This block is responsible for creation of EC2 Instance
-resource "aws_instance" "web-server-instance" {
-    ami = "ami-067f484fdbe6dd509" #Predefined AMI with instaled Apache that shows 'Well Done' message when connecting
-    instance_type = "t2.micro"
-    key_name = "SpaceliftKeyPair"
-    vpc_security_group_ids = [aws_security_group.httpconnection.id] #Connecting the instance to the internet
+resource "aws_s3_bucket" "my_bucket" {
+bucket = var.bucket_name
 
 tags = {
-    Name = "spacelift-course-server"
+Name        = "My S3 Bucket"
+Environment = "Dev"
 }
-}
-
-#This code allows us to connect the EC2 to the internet. Details of this block are not important right now.
-resource "aws_security_group" "httpconnection" {
-  description = "Allow incoming HTTP traffic on port 80"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
